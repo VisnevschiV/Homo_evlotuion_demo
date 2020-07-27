50dda8ecb6fcde7650104973e150491d2e9c9896
Vlad <visnevskivlad@gmail.com>
2020-07-27 15:10:03 +0300

    World-Menu animations completed

---
+++ Assets/Script/World-menu/UI_animator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UI_animator : MonoBehaviour
+{
+    public GameObject info;
+    public GameObject list;
+
+   public void onClick()
+    {
+       info.SetActive(true); 
+       LeanTween.scale(info , new Vector3(1f,1f,8), 1);
+       LeanTween.scale(list, new Vector3(0.9f,0.9f,4),1);
+
+    }
+    public void bckClick()
+    {
+        LeanTween.scale(info, new Vector3(0f,0f,8), 1);
+        LeanTween.scale(list, new Vector3(1.28f,1.28f,4),1);
+
+    }
+}
