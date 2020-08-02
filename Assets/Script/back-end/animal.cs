145bfd2e98bd9e71b37d3b9c8b169dab18a0dc88
Vlad <visnevskivlad@gmail.com>
2020-08-02 19:06:36 +0300

    nimic util

---
+++ Assets/Script/back-end/animal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animal : MonoBehaviour
+{
+    int species;
+    int nr;
+    int hp;
+    int speed;
+    int Damage;
+    int FightDistance;
+    int tacticsLVL;
+    int endurance;
+    bool aggresive;
+    bool scared;
+    
+
+
+}
---
+++ Assets/Script/back-end/evolutions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evolutions : MonoBehaviour
+{
+   
+    int i_creativity;
+    int i_speaking;
+    int i_safe_place;
+    int i_footprints;
+    int i_protection;
+    int i_teamfight;
+    int i_numbers;
+    int i_hierarchy;
+    int i_tactics;
+    int i_Religion;
+    int i_Stone;
+    int i_Stick;
+    int i_Fire;
+    int i_Weapons;
+    int i_Food;
+    int F_Biped;
+    int F_Mute;
+    int F_Danger;
+    int F_Power;
+    int F_Vision;
+    int F_Analyzing;
+    int F_Forest_vison;
+    int F_Running;
+    int F_Swimming;
+    int F_Endurance;
+    int F_Imunity;
+    int F_Regeneration;
+
+
+}
