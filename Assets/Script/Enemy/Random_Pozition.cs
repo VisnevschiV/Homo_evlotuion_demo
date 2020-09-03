56e6b14a4388be59e4b386200e3a99a9e60af0b1
Vlad <visnevskivlad@gmail.com>
2020-09-03 10:54:19 +0300

    2020.1 version + bear debugging & new bear design

--- Assets/Script/Enemy/Random_Pozition.cs
+++ Assets/Script/Enemy/Random_Pozition.cs
@@ -11,7 +11,7 @@ public class Random_Pozition : MonoBehaviour
     [SerializeField] private int minY;
     [SerializeField] private float speed;
 
-    [SerializeField] private GameObject _traces;
+    //[SerializeField] private GameObject _traces;
     private GameObject _player;
     [SerializeField] private float _distanceToAtack;
     [SerializeField] private int breakMaxX;
@@ -22,6 +22,10 @@ public class Random_Pozition : MonoBehaviour
     private int likelyToBreakTheLimit;
 
     private bool reachPoint = true;
+
+    private int i=0;
+  
+
 
     public void Start()
     {
@@ -37,52 +41,58 @@ public class Random_Pozition : MonoBehaviour
         }
         else
         {
-            likelyToBreakTheLimit = Random.Range(-1, 101);
-            if (likelyToBreakTheLimit <= 5)
+            
+            if(reachPoint == true)
             {
-                if (reachPoint == true)
-                {
-                    reachPoint = !reachPoint;
+               //the probability the bear leaves his natural place
+               reachPoint =false;
+               likelyToBreakTheLimit = Random.Range(-1, 101);
+               if (likelyToBreakTheLimit <= 1)
+               {
+                   //dif he does we use BreakMin/Max          
                     int breakX = Random.Range(breakMinX, breakMaxX);
                     int breakY = Random.Range(breakMinY, breakMaxY);
-                    StartCoroutine(StartMove(breakX, breakY));
-                }
-                else
-                {
-                    Starts();
-                    reachPoint = false;
-                }
-
-
-            }
-            else
-            {
-                if (reachPoint == true)
-                {
-
-                    reachPoint = !reachPoint;
+                    StartCoroutine(StartMove(breakX, breakY));  
+                }else
+               {
+                   // if he doesnt we go random
                     var randomX = Random.Range(minX, maxX);
                     var randomY = Random.Range(minY, maxY);
                     StartCoroutine(StartMove(randomX, randomY));
                 }
-                else
-                {
-                    Starts();
-                    reachPoint = false;
-                }
-
+            }else
+            {
+                //functia responsabila de urme
+                //Starts();
             }
         }
 
     }
 
+    // if he hits the map margins
+    void OnCollisionEnter2D(){
+        Debug.Log("water");
+        reachPoint = true;
+    }
+
     IEnumerator StartMove(int pozitionX, int pozitionY)
     {
-
         Vector2 distance = new Vector2(pozitionX, pozitionY);
-        while (Vector2.Distance(transform.position, distance) > 0.5f)
+        Debug.Log("X=" + pozitionX + " Y = " + pozitionY);
+     Checkpoint:
+        i=0;
+        while (/*Vector2.Distance(transform.position, distance) > 0.5f*/!reachPoint)
         {
+            i++;
             transform.position = Vector2.MoveTowards(transform.position, distance, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, distance) <= 0.5f){
+                reachPoint = true;
+            }
+            if(i > 100){
+                break;
+                goto Checkpoint;
+                Debug.Log("checkpoint");
+            }
             yield return null;
         }
         yield return new WaitForSeconds(1f);
@@ -92,14 +102,14 @@ public class Random_Pozition : MonoBehaviour
     public void Starts()
     {
         if (_finish == true)
-            StartCoroutine(Treas());
+            //StartCoroutine(Treas());
         _finish = false;
     }
 
-    IEnumerator Treas()
+   IEnumerator Treas()
     {
 
-        Instantiate(_traces, transform.position, transform.rotation);
+        //Instantiate(_traces, transform.position, transform.rotation);
         yield return new WaitForSeconds(0.5f);
         _finish = true;
 
